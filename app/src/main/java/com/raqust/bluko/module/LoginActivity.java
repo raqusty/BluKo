0781037743d7d79027b89a0c95c79a7c1367d44c
583006809@qq.com <linzehao123>
2017-12-06 15:04:36 +0800

    极光登录 分享

---
+++ app/src/main/java/com/raqust/bluko/module/LoginActivity.java
@@ -0,0 +1,82 @@
+package com.raqust.bluko.module;
+
+import android.view.View;
+import android.widget.TextView;
+
+import com.raqust.bluko.R;
+import com.raqust.bluko.common.activity.BaseActivity;
+import com.raqust.bluko.common.activity.ToolBarManager;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+/**
+ * Created by linzehao
+ * time: 2017/12/6.
+ * info:
+ */
+
+public class LoginActivity extends BaseActivity {
+
+    @BindView(R.id.text1)
+    TextView QQLogin;
+
+    @BindView(R.id.text2)
+    TextView WeiBoLogin;
+
+    @BindView(R.id.text3)
+    TextView WeiXinLogin;
+
+    @BindView(R.id.text4)
+    TextView QQShare;
+
+    @BindView(R.id.text5)
+    TextView WeiBoShare;
+
+    @BindView(R.id.text6)
+    TextView WeiXinShare;
+
+    @Override
+    public void initViews() {
+    }
+
+    @Override
+    public void setListener() {
+
+    }
+
+    @Override
+    public int getToolBarResId() {
+        return 0;
+    }
+
+    @Override
+    public void initToolBar(ToolBarManager navigationBarMgr) {
+
+    }
+
+    @Override
+    protected int getContentViewResId() {
+        return R.layout.activity_login;
+    }
+
+    @OnClick({R.id.text1, R.id.text2, R.id.text3, R.id.text4, R.id.text5, R.id.text6})
+    public void Click(View v) {
+        switch (v.getId()) {
+            case R.id.text1:
+                break;
+            case R.id.text2:
+                break;
+            case R.id.text3:
+                break;
+            case R.id.text4:
+                break;
+            case R.id.text5:
+                break;
+            case R.id.text6:
+                break;
+            default:
+                break;
+        }
+    }
+}
