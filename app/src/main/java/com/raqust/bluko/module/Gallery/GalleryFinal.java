e9c9a25c2f218fbb2f605d7e7adbd843ab7aabf6
583006809@qq.com <linzehao123>
2017-12-21 17:01:58 +0800

    极光登录 分享

---
+++ app/src/main/java/com/raqust/bluko/module/Gallery/GalleryFinal.java
@@ -0,0 +1,143 @@
+package com.raqust.bluko.module.Gallery;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.raqust.bluko.R;
+import com.raqust.bluko.common.activity.BaseActivity;
+import com.raqust.bluko.common.activity.ToolBarManager;
+import com.yalantis.ucrop.model.AspectRatio;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+import cn.finalteam.rxgalleryfinal.RxGalleryFinal;
+import cn.finalteam.rxgalleryfinal.bean.MediaBean;
+import cn.finalteam.rxgalleryfinal.imageloader.ImageLoaderType;
+import cn.finalteam.rxgalleryfinal.rxbus.RxBusResultDisposable;
+import cn.finalteam.rxgalleryfinal.rxbus.event.ImageMultipleResultEvent;
+import cn.finalteam.rxgalleryfinal.rxbus.event.ImageRadioResultEvent;
+
+/**
+ * Created by linzehao
+ * time: 2017/12/20.
+ * info:
+ */
+
+public class GalleryFinal extends BaseActivity {
+    @BindView(R.id.result)
+    TextView textView;
+    private List<MediaBean> list = null;
+
+    @Override
+    public void initViews() {
+
+    }
+
+    @Override
+    public void setListener() {
+
+    }
+
+    @Override
+    public int getToolBarResId() {
+        return 0;
+    }
+
+    @Override
+    public void initToolBar(ToolBarManager navigationBarMgr) {
+
+    }
+
+    @Override
+    protected int getContentViewResId() {
+        return R.layout.activity_gallery;
+    }
+
+    @OnClick({R.id.text1, R.id.text2, R.id.text3})
+    public void click(View v) {
+        switch (v.getId()) {
+            case R.id.text1: {
+                RxGalleryFinal
+                        .with(GalleryFinal.this)
+                        .image()
+                        .radio()
+                        .cropAspectRatioOptions(0, new AspectRatio("3:3", 30, 30))
+                        .crop()
+                        .imageLoader(ImageLoaderType.GLIDE)
+                        .subscribe(new RxBusResultDisposable<ImageRadioResultEvent>() {
+                            @Override
+                            protected void onEvent(ImageRadioResultEvent imageRadioResultEvent) throws Exception {
+                                Toast.makeText(GalleryFinal.this, "选中了图片路径：" + imageRadioResultEvent.getResult().getOriginalPath(), Toast.LENGTH_SHORT).show();
+                                textView.setText(imageRadioResultEvent.getResult().getOriginalPath());
+                            }
+                        })
+                        .openGallery();
+                break;
+            }
+            case R.id.text2: {
+                RxGalleryFinal rxGalleryFinal = RxGalleryFinal
+                        .with(GalleryFinal.this)
+                        .image()
+                        .multiple();
+                if (list != null && !list.isEmpty()) {
+                    rxGalleryFinal.selected(list);
+                }
+                rxGalleryFinal.maxSize(8)
+                        .imageLoader(ImageLoaderType.GLIDE)
+                        .subscribe(new RxBusResultDisposable<ImageMultipleResultEvent>() {
+
+                            @Override
+                            protected void onEvent(ImageMultipleResultEvent imageMultipleResultEvent) throws Exception {
+                                list = imageMultipleResultEvent.getResult();
+                                String str = "";
+                                for (MediaBean bean : list) {
+                                    Log.i("linzehao", bean.getOriginalPath());
+                                    str = str + bean.getOriginalPath() + "/r/n";
+                                }
+                                textView.setText(str);
+                                Toast.makeText(getBaseContext(), "已选择" + imageMultipleResultEvent.getResult().size() + "张图片", Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onComplete() {
+                                super.onComplete();
+                                Toast.makeText(getBaseContext(), "OVER", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .openGallery();
+                break;
+            }
+            case R.id.text3: {
+//                RxGalleryFinalApi
+//                        .getInstance(GalleryFinal.this)
+//                        .setType(RxGalleryFinalApi.SelectRXType.TYPE_VIDEO, RxGalleryFinalApi.SelectRXType.TYPE_SELECT_RADIO)
+//                        .setVDRadioResultEvent(new RxBusResultDisposable<ImageRadioResultEvent>() {
+//                            @Override
+//                            protected void onEvent(ImageRadioResultEvent imageRadioResultEvent) throws Exception {
+//                                Toast.makeText(getApplicationContext(), imageRadioResultEvent.getResult().getOriginalPath(), Toast.LENGTH_SHORT).show();
+//                            }
+//                        })
+//                        .open();
+
+                RxGalleryFinal.with(GalleryFinal.this)
+                        .radio()//单选
+                        .video()//视频
+                        .imageLoader(ImageLoaderType.GLIDE)
+                        //里面可以选择主流图片工具  PICASSO  GLIDE  FRESCO UNIVERSAL(ImageLoader)
+                        .subscribe(new RxBusResultDisposable<ImageRadioResultEvent>() {
+                            @Override
+                            protected void onEvent(ImageRadioResultEvent imageRadioResultEvent) throws Exception {
+                                Toast.makeText(getApplicationContext(), imageRadioResultEvent.getResult().getOriginalPath(), Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .openGallery();
+                break;
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/raqust/bluko/module/ScrollToolActivity/ScrollToolActivity.java
@@ -0,0 +1,37 @@
+package com.raqust.bluko.module.ScrollToolActivity;
+
+import com.raqust.bluko.common.activity.BaseActivity;
+import com.raqust.bluko.common.activity.ToolBarManager;
+
+/**
+ * Created by linzehao
+ * time: 2017/12/21.
+ * info:
+ */
+
+public class ScrollToolActivity extends BaseActivity {
+    @Override
+    public void initViews() {
+
+    }
+
+    @Override
+    public void setListener() {
+
+    }
+
+    @Override
+    public int getToolBarResId() {
+        return 0;
+    }
+
+    @Override
+    public void initToolBar(ToolBarManager navigationBarMgr) {
+
+    }
+
+    @Override
+    protected int getContentViewResId() {
+        return 0;
+    }
+}
