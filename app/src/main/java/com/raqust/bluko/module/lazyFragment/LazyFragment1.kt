a5b9ce7b67917c699954eef29f9c0248036598d2
林泽豪 <linzehao@xingfeiinc.com>
2018-05-21 16:56:39 +0800

    删除多余的东西

--- app/src/main/java/com/raqust/bluko/module/lazyFragment/LazyFragment1.kt
+++ app/src/main/java/com/raqust/bluko/module/lazyFragment/LazyFragment1.kt
@@ -45,7 +45,7 @@ class LazyFragment1 : BaseFragment(){
     override fun initViews(view: View) {
         Log.i("linzehao", "initViews 1")
         var pager2 = view.findViewById(R.id.pager2) as ViewPager
-        pager2.adapter = object : FragmentPagerAdapter(activity.supportFragmentManager) {
+        pager2.adapter = object : FragmentPagerAdapter(activity?.supportFragmentManager) {
             override fun getCount() = fragments.size
             override fun getItem(position: Int) = fragments[position]
             override fun getPageTitle(position: Int) = ""
