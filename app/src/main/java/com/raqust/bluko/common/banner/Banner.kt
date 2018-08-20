6f0ad9e16b5730566c587c731327174e0b7aa3a0
林泽豪 <linzehao123>
2018-08-20 16:40:25 +0800

    banner

--- app/src/main/java/com/raqust/bluko/common/banner/Banner.kt
+++ app/src/main/java/com/raqust/bluko/common/banner/Banner.kt
@@ -6,6 +6,7 @@ import android.support.v4.view.ViewPager
 import android.util.AttributeSet
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.View.OnClickListener
 import android.view.ViewGroup
@@ -14,8 +15,6 @@ import android.widget.ImageView
 import com.raqust.bluko.R
 import com.raqust.bluko.common.banner.listener.OnBannerListener
 import com.raqust.bluko.common.banner.loader.ImageLoader
-import com.raqust.bluko.common.banner.loader.ImageLoaderInterface
-import com.raqust.bluko.module.testActivity.banner.GlideImageLoader
 
 /**
  * Created by linzehao
@@ -39,8 +38,9 @@ class Banner : FrameLayout, ViewPager.OnPageChangeListener {
     private val imageViews = mutableListOf<View>()
     private var imageUrls: MutableList<*>? = null
     private val adapter by lazy { BannerPagerAdapter() }
-    private var imageLoader: ImageLoader?= null
+    private var imageLoader: ImageLoader? = null
     private var mOnPageChangeListener: ViewPager.OnPageChangeListener? = null
+    private val handler = WeakHandler()
 
     private var currentItem: Int = 1
     private var count = 0
@@ -84,20 +84,26 @@ class Banner : FrameLayout, ViewPager.OnPageChangeListener {
         mViewPager?.adapter = adapter
         mViewPager?.isFocusable = true
         mViewPager?.currentItem = 1
-//        if (gravity != -1)
-//            indicator.setGravity(gravity)
         if (isScroll && count > 1) {
             mViewPager?.setScrollable(true)
         } else {
             mViewPager?.setScrollable(false)
         }
-//        if (isAutoPlay)
-//            startAutoPlay()
+        if (isAutoPlay)
+            startAutoPlay()
     }
 
+    fun startAutoPlay() {
+        handler.removeCallbacks(task)
+        handler.postDelayed(task, delayTime.toLong())
+    }
+
+    fun stopAutoPlay() {
+        handler.removeCallbacks(task)
+    }
 
     private fun setImageList(imagesUrl: List<*>?) {
-        if (imagesUrl == null || imagesUrl.size <= 0) {
+        if (imagesUrl == null || imagesUrl.isEmpty()) {
             Log.e(tag, "The image data set is empty.")
             return
         }
@@ -125,6 +131,21 @@ class Banner : FrameLayout, ViewPager.OnPageChangeListener {
         }
     }
 
+    private val task = object : Runnable {
+        override fun run() {
+            if (count > 1 && isAutoPlay) {
+                currentItem = currentItem % (count + 1) + 1
+                //                Log.i(tag, "curr:" + currentItem + " count:" + count);
+                if (currentItem == 1) {
+                    mViewPager?.setCurrentItem(currentItem, false)
+                    handler.post(this)
+                } else {
+                    mViewPager?.currentItem = currentItem
+                    handler.postDelayed(this, delayTime.toLong())
+                }
+            }
+        }
+    }
 
     internal inner class BannerPagerAdapter : PagerAdapter() {
 
@@ -157,7 +178,7 @@ class Banner : FrameLayout, ViewPager.OnPageChangeListener {
     }
 
     /**
-     *
+     * banner图片点击事件
      */
     fun setOnBannerListener(listener: OnBannerListener): Banner {
         this.listener = listener
@@ -165,7 +186,7 @@ class Banner : FrameLayout, ViewPager.OnPageChangeListener {
     }
 
     /**
-     *
+     * 设置图片的链接
      */
     fun setImages(imageUrls: MutableList<*>): Banner {
         this.imageUrls = imageUrls
@@ -174,7 +195,7 @@ class Banner : FrameLayout, ViewPager.OnPageChangeListener {
     }
 
     /**
-     *
+     * 设置可否点击
      */
     fun setViewPagerIsScroll(isScroll: Boolean): Banner {
         this.isScroll = isScroll
@@ -182,18 +203,26 @@ class Banner : FrameLayout, ViewPager.OnPageChangeListener {
     }
 
     /**
-     *
+     * 设置是否自动播放
      */
     fun isAutoPlay(isAutoPlay: Boolean): Banner {
         this.isAutoPlay = isAutoPlay
         return this
     }
+
+    /**
+     * 设置延时的时间
+     */
+    fun setDelayTime(delayTime: Int): Banner {
+        this.delayTime = delayTime
+        return this
+    }
+
     override fun onPageScrollStateChanged(state: Int) {
 
         if (mOnPageChangeListener != null) {
             mOnPageChangeListener?.onPageScrollStateChanged(state)
         }
-//        Log.i(tag,"currentItem: "+currentItem);
         when (state) {
             0//No operation
             -> if (currentItem == 0) {
@@ -219,7 +248,7 @@ class Banner : FrameLayout, ViewPager.OnPageChangeListener {
         }
     }
 
-    override fun onPageSelected( index: Int) {
+    override fun onPageSelected(index: Int) {
         var position = index
         currentItem = position
         if (mOnPageChangeListener != null) {
@@ -252,6 +281,21 @@ class Banner : FrameLayout, ViewPager.OnPageChangeListener {
         if (realPosition < 0)
             realPosition += count
         return realPosition
+    }
+
+
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        //        Log.i(tag, ev.getAction() + "--" + isAutoPlay);
+        if (isAutoPlay) {
+            val action = ev.action
+            if (action == MotionEvent.ACTION_UP || action == MotionEvent.ACTION_CANCEL
+                    || action == MotionEvent.ACTION_OUTSIDE) {
+                startAutoPlay()
+            } else if (action == MotionEvent.ACTION_DOWN) {
+                stopAutoPlay()
+            }
+        }
+        return super.dispatchTouchEvent(ev)
     }
 
 }
